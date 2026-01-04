cbf1c5cb0b12e8cffe163129213bb10a941f5c56
Ippei Nagamine <1pei.nag@gmail.com>
2026-01-04 09:12:50 +0900

    Initial commit: Vite + Capacitor setup

---
+++ android/app/src/main/java/com/retrogamecollection/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.retrogamecollection.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.retrogamecollection.app',
+  appName: 'Retro Game Collection',
+  webDir: 'dist'
+};
+
+export default config;
